fde1fb8b22efe4b681c9018a1f3165f96736621a
Spencer Schack <scs1818@gmail.com>
2014-01-06 13:33:41 -0800

    removed spree init for now

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,3 +1,4 @@
+=begin
 # Configure Spree Preferences
 #
 # Note: Initializing preferences available within the Admin will overwrite any changes that were made through the user interface when you restart.
@@ -12,3 +13,4 @@ Spree.config do |config|
 end
 
 Spree.user_class = "Spree::User"
+=end
