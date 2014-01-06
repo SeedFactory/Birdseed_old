c81096e93f3dbf8176f195b0f22b9cabdb299f7b
Spencer Schack <scs1818@gmail.com>
2014-01-06 11:59:15 -0800

    Fresh rails 4.0.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Birdseed::Application.config.secret_key_base = '2a4bd138b41056392949036734b1e7e4a97b7ddc49243721ded9652230d9c47e64a9efe84ddb68b8b018488b7554ee1e9c4ac16ee77e6904e06a5951b08a2305'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Birdseed::Application.config.session_store :cookie_store, key: '_Birdseed_session'
