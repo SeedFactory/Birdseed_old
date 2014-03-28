a5d9d295f8d1a44cb8dbaf75c1e20ecbf9b54323
Spencer Schack <scs1818@gmail.com>
2014-03-28 10:22:43 -0700

    removed s3 settings from spree config because it is also in the frontend configuration

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -6,13 +6,7 @@
 # In order to initialize a setting do:
 # config.setting_name = 'new value'
 Spree.config do |config|
-  # Example:
-  # Uncomment to override the default site name.
-  # config.site_name = "Spree Demo Site"
-  config.use_s3 = true
-  config.s3_access_key = ENV['AWS_ACCESS_KEY_ID']
-  config.s3_secret = ENV['AWS_SECRET_ACCESS_KEY']
-  config.s3_bucket = 'birdseed'
+  
 end
 
 Spree.user_class = "Spree::User"
